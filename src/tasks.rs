0039639099cf735ce88b0ac44b3e61fa3ff2aad7
Michael Guzman <miggielito@Miggies-MacBook-Pro.local>
2022-06-05 18:03:17 -0700

    added complet5e task function

--- src/tasks.rs
+++ src/tasks.rs
@@ -2,7 +2,7 @@ use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
 use std::fs::{File, OpenOptions};
-use std::io::{Result, Seek, SeekFrom};
+use std::io::{Error, ErrorKind, Result, Seek, SeekFrom}; // Include the `Error` type.
 use std::path::PathBuf;
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -28,15 +28,7 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
         .create(true)
         .open(journal_path)?;
 
-    // Consume the file's contents as a vector of tasks.
-    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
-        Ok(tasks) => tasks,
-        Err(e) if e.is_eof() => Vec::new(),
-        Err(e) => Err(e)?,
-    };
-
-    // Rewind the file after reading from it.
-    file.seek(SeekFrom::Start(0))?;
+    let mut tasks = collect_tasks(&file)?;
 
     // Write the modified task list back into the file.
     tasks.push(task);
@@ -46,9 +38,38 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
 }
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
-    unimplemented!()
+    // Open the file.
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(journal_path)?;
+
+    let mut tasks = collect_tasks(&file)?;
+    // Remove the task.
+    if task_position == 0 || task_position > tasks.len() {
+        return Err(Error::new(ErrorKind::InvalidInput, "Invalid Task ID"));
+    }
+    tasks.remove(task_position - 1);
+
+    // Rewind and truncate the file.
+    file.set_len(0)?;
+
+    // Write the modified task list back into the file.
+    serde_json::to_writer(file, &tasks)?;
+    Ok(())
 }
 
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
     unimplemented!()
+}
+
+fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
+    file.seek(SeekFrom::Start(0))?; // Rewind the file before.
+    let tasks = match serde_json::from_reader(file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+    file.seek(SeekFrom::Start(0))?; // Rewind the file after.
+    Ok(tasks)
 }
