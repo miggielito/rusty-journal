c1782cce443dda5dc8950bf5162249f195e6241d
Michael Guzman <miggielito@Miggies-MacBook-Pro.local>
2022-06-11 19:41:36 -0700

    to-do list final pro

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,33 @@ mod cli;
 mod tasks;
 use structopt::StructOpt;
 
-fn main() {
-    cli::CommandLineArgs::from_args();
+use cli::{Action::*, CommandLineArgs};
+use std::path::PathBuf;
+use tasks::Task;
+
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+
+fn main() -> anyhow::Result<()> {
+    // Get the command-line arguments.
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = CommandLineArgs::from_args();
+
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file.");
+
+    // Perform the action.
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }?;
+    Ok(())
 }
--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,7 @@
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::fmt;
 use std::fs::{File, OpenOptions};
 use std::io::{Error, ErrorKind, Result, Seek, SeekFrom}; // Include the `Error` type.
 use std::path::PathBuf;
@@ -20,9 +21,15 @@ impl Task {
         }
     }
 }
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let created_at = self.created_at.with_timezone(&Local).format("%F %H:%M");
+        write!(f, "{:<50} [{}]", self.text, created_at)
+    }
+}
 pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
     // Open the file.
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
@@ -39,7 +46,7 @@ pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
 
 pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
     // Open the file.
-    let mut file = OpenOptions::new()
+    let file = OpenOptions::new()
         .read(true)
         .write(true)
         .open(journal_path)?;
@@ -60,7 +67,23 @@ pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> 
 }
 
 pub fn list_tasks(journal_path: PathBuf) -> Result<()> {
-    unimplemented!()
+    // Open the file.
+    let file = OpenOptions::new().read(true).open(journal_path)?;
+    // Parse the file and collect the tasks.
+    let tasks = collect_tasks(&file)?;
+
+    // Enumerate and display tasks, if any.
+    if tasks.is_empty() {
+        println!("Task list is empty!");
+    } else {
+        let mut order: u32 = 1;
+        for task in tasks {
+            println!("{}: {}", order, task);
+            order += 1;
+        }
+    }
+
+    Ok(())
 }
 
 fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
@@ -70,6 +93,7 @@ fn collect_tasks(mut file: &File) -> Result<Vec<Task>> {
         Err(e) if e.is_eof() => Vec::new(),
         Err(e) => Err(e)?,
     };
+
     file.seek(SeekFrom::Start(0))?; // Rewind the file after.
     Ok(tasks)
 }
